2eb81c623e19eb0064e4905728bbd978c2242e9d
happy_ryo <iwama.ryo.0731@gmail.com>
2013-12-23 16:44:24 +0900

    Facebook permissions のテストを書いた

---
+++ checon/MODELS/FacebookPermissions.m
@@ -0,0 +1,16 @@
+//
+// Created by happy_ryo on 2013/12/23.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+#import "FacebookPermissions.h"
+
+
+@implementation FacebookPermissions {
+
+}
+
+- (NSArray *)permissions {
+    return @[@"user_checkins", @"publish_checkins"];
+}
+@end
