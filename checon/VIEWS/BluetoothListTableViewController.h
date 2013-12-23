cf6be635e66f505717543517e9cf32d42323da5d
happy_ryo <iwama.ryo.0731@gmail.com>
2013-12-24 00:08:39 +0900

    表示用のテーブルクラスを追加した

---
+++ checon/VIEWS/BluetoothListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BluetoothListTableViewController.h
+//  checon
+//
+//  Created by happy_ryo on 2013/12/23.
+//
+//
+
+
+
+@interface BluetoothListTableViewController : UITableViewController
+
+@end
