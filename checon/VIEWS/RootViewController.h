b6c6ec6904b0bda576af52df306602c8d5e0d01e
happy_ryo <iwama.ryo.0731@gmail.com>
2013-12-23 16:46:46 +0900

    画面構成を軽く作った

---
+++ checon/VIEWS/RootViewController.h
@@ -0,0 +1,4 @@
+//  Copyright 2013 happy_ryo
+
+@interface RootViewController : UIViewController
+@end
---
+++ checon/VIEWS/RootViewController.m
@@ -0,0 +1,19 @@
+//  Copyright 2013 happy_ryo
+
+#import "RootViewController.h"
+
+@implementation RootViewController {
+
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    [super prepareForSegue:segue sender:sender];
+}
+
+@end
